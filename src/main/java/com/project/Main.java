81e06769b698c0f07aad72333df2176b0679e7a8
RufanNiyazli <developerx73@gmail.com>
2025-10-30 21:49:35 +0400

    minesweeper

---
+++ src/main/java/com/project/Main.java
@@ -0,0 +1,10 @@
+package com.project;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Minesweeper minesweeper = new Minesweeper();
+
+    }
+}
